59cce3e2b527bc22fff8357c2646a2a2912219c3
Yanel Gomez <yanig_04@hotmail.com>
2022-08-14 19:14:22 -0300

    se agrega backend

---
+++ backend/app.js
@@ -0,0 +1,73 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter=require('./routes/admin/login');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin/login',loginRouter);
+
+app.use(session({
+  secret: 'hsdhuyrbfjkfkkdfsjad',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use('/', indexRouter);
+
+app.get('/', function (req, res) {
+  var conocido = Boolean(req.session.nombre);
+
+  res.render('index', {
+    title: 'Sesiones en Express.js',
+    conocido: conocido,
+    nombre: req.session.nombre
+  });
+});
+
+app.post('ingresar', function (req, res) {
+  if (req.body.nombre) {
+    req.session.nombre = req.body.nombre
+  }
+  res.redirect('/');
+});
+
+app.get('/salir',function (req, res) {
+  req.session.destroy();
+  res.redirect('/')
+});
+
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
